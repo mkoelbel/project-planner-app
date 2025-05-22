064aa4d29da41d6b08820aefa79c86812a0b9ab1
Maria Koelbel <mariackoelbel@gmail.com>
2025-05-22 12:29:31 -0400

    Make sidebar sticky so it doesn't look weird when page scrolls down

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,25 @@ import ProjectDashboard from 'pages/ProjectDashboard';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
-const { Sider, Header, Content } = Layout; 
+const { Sider, Header, Content } = Layout;
 
 const App = () => {
   const navigate = useNavigate();
 
   return (
-    <Layout>
+    <Layout hasSider>
       {/* Sidebar */}
       <Sider collapsible>
-        <Menu 
-          theme="dark" 
-          mode="inline" 
-          defaultSelectedKeys={['project-dashboard']}
-          onClick={({ key }) => navigate(`/${key}`)}
-        >
-          <Menu.Item key="project-dashboard" icon={<ProjectOutlined />}>Project Dashboard</Menu.Item>
-          <Menu.Item key="project-management" icon={<PlusCircleOutlined />}>Project Management</Menu.Item>
-          <Menu.Item key="user-management" icon={<UserOutlined />}>User Management</Menu.Item>
-        </Menu>
+          <Menu 
+            theme="dark" 
+            mode="inline" 
+            defaultSelectedKeys={['project-dashboard']}
+            onClick={({ key }) => navigate(`/${key}`)}
+          >
+            <Menu.Item key="project-dashboard" icon={<ProjectOutlined />}>Project Dashboard</Menu.Item>
+            <Menu.Item key="project-management" icon={<PlusCircleOutlined />}>Project Management</Menu.Item>
+            <Menu.Item key="user-management" icon={<UserOutlined />}>User Management</Menu.Item>
+          </Menu>
       </Sider>
 
       <Layout>
