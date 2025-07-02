a0faf128322fb1c7747d951fc16aed6f8153968f
Maria Koelbel <mariackoelbel@gmail.com>
2025-07-02 13:48:16 -0400

    Add Strict Mode to app

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from '@ant-design/icons';
 import { Layout, Menu, Typography } from 'antd';
 import ProjectDashboard from 'pages/ProjectDashboard';
+import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
@@ -14,37 +15,39 @@ const App = () => {
   const navigate = useNavigate();
 
   return (
-    <Layout>
-      {/* Header */}
-      <Header>
-        <Typography.Title level={2}>XYZ Company Project Planner</Typography.Title>
-      </Header>
-
+    <React.StrictMode>
       <Layout>
-        {/* Sidebar */}
-        <Sider collapsible>
-            <Menu 
-              theme="dark" 
-              mode="inline" 
-              defaultSelectedKeys={['project-dashboard']}
-              onClick={({ key }) => navigate(`/${key}`)}
-            >
-              <Menu.Item key="project-dashboard" icon={<ProjectOutlined />}>Project Dashboard</Menu.Item>
-              <Menu.Item key="project-management" icon={<PlusCircleOutlined />}>Project Management</Menu.Item>
-              <Menu.Item key="user-management" icon={<UserOutlined />}>User Management</Menu.Item>
-            </Menu>
-        </Sider>
+        {/* Header */}
+        <Header>
+          <Typography.Title level={2}>XYZ Company Project Planner</Typography.Title>
+        </Header>
 
-        {/* Main Layout */}
-        <Content>
-          <Routes>
-            <Route path="/project-dashboard" element={<ProjectDashboard />}></Route>
-            <Route path="/project-management" element={<div>Tasks Page (Coming Soon)</div>} />
-            <Route path="/user-management" element={<div>Teams Page (Coming Soon)</div>} />
-          </Routes>
-        </Content>
+        <Layout>
+          {/* Sidebar */}
+          <Sider collapsible>
+              <Menu 
+                theme="dark" 
+                mode="inline" 
+                defaultSelectedKeys={['project-dashboard']}
+                onClick={({ key }) => navigate(`/${key}`)}
+              >
+                <Menu.Item key="project-dashboard" icon={<ProjectOutlined />}>Project Dashboard</Menu.Item>
+                <Menu.Item key="project-management" icon={<PlusCircleOutlined />}>Project Management</Menu.Item>
+                <Menu.Item key="user-management" icon={<UserOutlined />}>User Management</Menu.Item>
+              </Menu>
+          </Sider>
+
+          {/* Main Layout */}
+          <Content>
+            <Routes>
+              <Route path="/project-dashboard" element={<ProjectDashboard />}></Route>
+              <Route path="/project-management" element={<div>Tasks Page (Coming Soon)</div>} />
+              <Route path="/user-management" element={<div>Teams Page (Coming Soon)</div>} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </React.StrictMode>
   );
 };
 
